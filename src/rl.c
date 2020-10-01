63f9f1f779bbc2d554df970cf8aadb7642bb847a
M3nti0nl3ss <ayoub-sifo@live.fr>
2020-10-01 17:01:53 +0100

    just a commit

--- src/rl.c
+++ src/rl.c
@@ -1,14 +1,14 @@
 #include"rl.h"
 
 int takeInput(char* str) { 
-	char buf[MAXCOM]; 
+    char buf[MAXCOM]; 
     printf("\n$> ");
     fgets(buf, sizeof(buf), stdin);//TODO - Change later
     int len=strlen(buf);
-	if (len != 0) {
+    if (len != 0) {
         buf[len-1]=0x20;
-		add_history(buf);
-		strcpy(str, buf); 
-		return 0; 
-	} else return 1; 
+        add_history(buf);
+        strcpy(str, buf); 
+        return 0; 
+    } else return 1; 
 }
